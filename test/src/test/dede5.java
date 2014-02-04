432ed332f4e1cdf73f548c0aed48d09c5341123e
stonesong <chans.pierre@gmail.com>
2014-02-04 10:56:11 +0100

    test12: renommage sur deux commit

---
+++ test/src/test/dede5.java
@@ -0,0 +1,29 @@
+package test;
+
+public class dudu {
+	
+	
+	static String d = " henry";
+	
+	
+	public dudu(){
+	
+		
+		d += " dudu";
+	
+		
+	}
+	
+	
+	public String getS(){
+	
+		d+=" haha";
+		
+		
+		
+		return d;
+	
+	}
+
+	
+}
