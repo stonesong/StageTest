dbc3295170caeb8f14a67a8bc32fe105d47cdde1
stonesong <chans.pierre@gmail.com>
2014-02-04 10:51:06 +0100

    test11: des sauts de lignes (tj rename pas reconnu)

--- test/src/test/dede4.java
+++ test/src/test/dede4.java
@@ -2,19 +2,28 @@ package test;
 
 public class dudu {
 	
+	
 	static String d = " henry";
 	
+	
 	public dudu(){
+	
 		
 		d += " dudu";
 	
+		
 	}
 	
+	
 	public String getS(){
+	
 		d+=" haha";
 		
 		
+		
 		return d;
+	
 	}
 
+	
 }
