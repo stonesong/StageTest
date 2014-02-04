14621741e4477f0a3d8f7c62d71030687de6a74c
stonesong <chans.pierre@gmail.com>
2014-02-04 10:47:33 +0100

    test9: des espaces et saut de lignes

---
+++ test/src/test/dede2.java
@@ -0,0 +1,20 @@
+package test;
+
+public class dudu {
+	
+	static String d = "                                                                   henry";
+	
+	public dudu(){
+		
+		d += "                                           dudu";
+	
+	}
+	
+	public String getS(){
+		d+="                        haha";
+		
+		
+		return d;
+	}
+
+}
