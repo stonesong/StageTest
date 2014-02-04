8860a73367b99bafc6ab98c5d9bf99232372a324
stonesong <chans.pierre@gmail.com>
2014-02-04 11:19:08 +0100

    test13: espaces dans une String

---
+++ test/src/test/dede6.java
@@ -0,0 +1,34 @@
+package test;
+
+public class dede6 {
+
+}
+package test;
+
+public class dudu {
+	
+	
+	static String d = "                                                                                                                            henry";
+	
+	
+	public dudu(){
+	
+		
+		d += " dudu";
+	
+		
+	}
+	
+	
+	public String getS(){
+	
+		d+=" haha";
+		
+		
+		
+		return d;
+	
+	}
+
+	
+}
