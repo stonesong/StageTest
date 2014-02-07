9de5ce7c497c927dc897b7ad0d1ef827d07bd477
stonesong <chans.pierre@gmail.com>
2014-02-07 11:26:26 +0100

    test deplacement dans packages

---
+++ test/src/bob/henry4.java
@@ -0,0 +1,31 @@
+package bob;
+
+public class henry4 {
+
+
+	public class dudu {
+
+
+		static String d = "                                                                                                                            henry";
+
+
+		public dudu(){
+
+
+			d += " dudu";
+
+
+		}
+
+		public String getS(){
+
+			d+=" haha";
+
+
+
+			return d;
+
+		}
+
+	}
+}
