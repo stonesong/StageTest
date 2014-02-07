839bd5883bde152759efcba3e819c20e2fb10e8d
stonesong <chans.pierre@gmail.com>
2014-02-07 11:19:37 +0100

    test undo

---
+++ test/src/bob/dede7.java
@@ -0,0 +1,36 @@
+package test;
+
+public class dede6 {
+
+}
+package bob;
+
+public String getS(){
+	
+	d+=" haha";
+	
+	
+	
+	return d;
+
+}
+
+public class dudu {
+	
+	
+	static String d = "                                                                                                                            henry";
+	
+	
+	public dudu(){
+	
+		
+		d += " dudu";
+	
+		
+	}
+	
+	
+	
+
+	
+}
