df9f9db559e5023cc63442f1bf9d7ad4e0297112
stonesong <chans.pierre@gmail.com>
2014-02-07 11:23:55 +0100

    test deplacement fonction dans fichier

---
+++ test/src/test/henry.java
@@ -0,0 +1,31 @@
+package test;
+
+public class henry {
+
+
+	public class dudu {
+
+
+		static String d = "                                                                                                                            henry";
+
+
+		public dudu(){
+
+
+			d += " dudu";
+
+
+		}
+	public String getS(){
+
+		d+=" haha";
+
+
+
+		return d;
+
+	}
+
+
+	}
+}
