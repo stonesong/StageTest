707b433d7bc139794df61ad3465298938d815302
stonesong <chans.pierre@gmail.com>
2014-02-07 11:22:54 +0100

    test deplacement fonction dans fichier

--- test/src/test/dede7.java
+++ test/src/test/dede7.java
@@ -2,35 +2,30 @@ package test;
 
 public class dede6 {
 
-}
-package test;
 
-public String getS(){
-	
-	d+=" haha";
-	
-	
-	
-	return d;
 
-}
+	public String getS(){
 
-public class dudu {
-	
-	
-	static String d = "                                                                                                                            henry";
-	
-	
-	public dudu(){
-	
-		
-		d += " dudu";
-	
-		
+		d+=" haha";
+
+
+
+		return d;
+
 	}
-	
-	
-	
 
-	
+	public class dudu {
+
+
+		static String d = "                                                                                                                            henry";
+
+
+		public dudu(){
+
+
+			d += " dudu";
+
+
+		}
+	}
 }
